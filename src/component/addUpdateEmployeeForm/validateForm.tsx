a0680d6349a72ab5c4f6387b691f1451cd4a8a6b
nidhi gupta <nidhitech0510@gmail.com>
2024-03-29 17:21:14 +0530

    validation change

--- src/component/addUpdateEmployeeForm/validateForm.tsx
+++ src/component/addUpdateEmployeeForm/validateForm.tsx
@@ -11,26 +11,75 @@ interface EmpDataErr {
     terms?: string
 }
 
-export const validateForm = (empFilledData: Employee): EmpDataErr => {
+// export const validateForm = (empFilledData: Employee): EmpDataErr => {
+//     const empDataError = {}
+//     console.log(empFilledData)
+//     if (empFilledData.firstName === "") {
+//         return { ...empDataError, ["firstName"]: "First Name is required" }
+//     } else if (empFilledData.lastName === "") {
+//         return { ...empDataError, ["lastName"]: "Last Name is required" }
+//     } else if (empFilledData.email === "") {
+//         return { ...empDataError, ["email"]: "Email is required" }
+//     } else if (empFilledData.dob === new Date()) {
+//         return { ...empDataError, ["dob"]: "Date of birth is required" }
+//     } else if (empFilledData.address === "") {
+//         return { ...empDataError, ["address"]: "Address is required" }
+//     } else if (empFilledData.phone === "") {
+//         return { ...empDataError, ["phone"]: "Phone number is required" }
+//     } else if (empFilledData.gender === "") {
+//         return { ...empDataError, ["gender"]: "Gender is required" }
+//     } else if (empFilledData.terms === false) {
+//         return { ...empDataError, ["terms"]: "Terms needs to be accepted" }
+//     }
+//     return empDataError
+// }
 
-    const empDataError = {}
-    console.log(empFilledData)
-    if (empFilledData.firstName === "") {
-        return { ...empDataError, ["firstName"]: "First Name is required" }
-    } else if (empFilledData.lastName === "") {
-        return { ...empDataError, ["lastName"]: "Last Name is required" }
-    } else if (empFilledData.email === "") {
-        return { ...empDataError, ["email"]: "email is required" }
-    } else if (empFilledData.dob === new Date()) {
-        return { ...empDataError, ["dob"]: "date of birth is required" }
-    } else if (empFilledData.address === "") {
-        return { ...empDataError, ["address"]: "Address is required" }
-    } else if (empFilledData.phone === "") {
-        return { ...empDataError, ["phone"]: "Phone is required" }
-    } else if (empFilledData.gender === "") {
-        return { ...empDataError, ["gender"]: "Gender is required" }
-    } else if (empFilledData.terms === false) {
-        return { ...empDataError, ["terms"]: "terms need to be accepted" }
-    }
+export const validateForm = (empFilledData: any): EmpDataErr => {
+    let empDataError = {}
+    let validateObj = [
+        {
+            name: "firstName",
+            equals: ""
+        },
+        {
+            name: "lastName",
+            equals: ""
+        },
+        {
+            name: "email",
+            equals: ""
+        },
+        {
+            name: "dob",
+            equals: null
+        },
+        {
+            name: "address",
+            equals: ""
+        },
+        {
+            name: "phone",
+            equals: ""
+        },
+        {
+            name: "gender",
+            equals: ""
+        },
+        {
+            name: "terms",
+            equals: false
+        }
+    ]
+
+    validateObj.map(each => {
+
+        if (empFilledData[each.name] === each.equals) {
+            empDataError = {
+                ...empDataError,
+                [each.name]: "required"
+            }
+        }
+    })
+    console.log(empDataError)
     return empDataError
 }
